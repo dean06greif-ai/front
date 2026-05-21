e284135d708baf66fb8af0bca2c9f17a662a2a11
Dean <dean06greif@gmail.com>
2026-05-21 21:38:51 +0200

    Initial backend commit

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
-export const API = `${BACKEND_URL}/api`;
+export const API = BACKEND_URL;
 
 export const api = axios.create({
   baseURL: API,
@@ -11,6 +11,6 @@ export const api = axios.create({
 export const wsUrl = () => {
   const url = new URL(BACKEND_URL);
   const proto = url.protocol === "https:" ? "wss:" : "ws:";
-  return `${proto}//${url.host}/api/ws`;
+  return `${proto}//${url.host}/ws`;
 };
 
